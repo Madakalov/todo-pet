b7940cd7113a312b5bffc6ed6ad5f15333861e81
Egor Madakalov <e.madakalov@yandex.ru>
2024-09-24 01:42:56 +0500

    Запрос на сервер

--- src/getsrp.js
+++ src/getsrp.js
@@ -1,1 +1,12 @@
-console.log('Hi!')
+async function getResponse() {
+    const response = await fetch(
+        "https://petstore3.swagger.io/api/v3/pet/findByStatus?status=available"
+    );
+    const json = await response.json();
+    const sliceJson = json.slice(0, 15);
+    for (let key in sliceJson) {
+        console.log(sliceJson[key])
+    }
+}
+
+getResponse()
