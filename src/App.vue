f25caa79eb5a0e5f4bf81c127fff47d24b830396
Egor Madakalov <e.madakalov@yandex.ru>
2024-09-23 22:38:00 +0500

    Верстка и стилизация тудушки

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,73 @@
         <div class="todo__inner">
             <div class="todo__task task">
                 <input type="text" class="task__input" />
-                <button class="task__add button"></button>
-                <button class="task__delete button"></button>
+                <div class="task__action">
+                    <button class="task__add button">ДОБАВИТЬ</button>
+                    <button class="task__clean button">ОЧИСТИТЬ</button>
+                </div>
             </div>
             <ul class="todo__list">
                 <li class="todo__item">
                     <div class="todo__container">
                         <input
                             type="checkbox"
-                            name=""
-                            id=""
+                            name="todo-checkbox"
+                            id="todo-checkbox"
                             class="todo__checkbox"
+                            title="Отметить выполнение"
                         />
-                        <p class="todo__content"></p>
+                        <p class="todo__content">
+                            Далеко-далеко за словесными горами в стране.
+                        </p>
                         <button
                             type="button"
                             class="todo__delete button"
                             id="todo__delete"
-                        ></button>
+                        >
+                            УДАЛИТЬ
+                        </button>
+                    </div>
+                </li>
+                <li class="todo__item">
+                    <div class="todo__container">
+                        <input
+                            type="checkbox"
+                            name="todo-checkbox"
+                            id="todo-checkbox"
+                            class="todo__checkbox"
+                            title="Отметить выполнение"
+                        />
+                        <p class="todo__content">
+                            Далеко-далеко за словесными горами в стране.
+                        </p>
+                        <button
+                            type="button"
+                            class="todo__delete button"
+                            id="todo__delete"
+                        >
+                            УДАЛИТЬ
+                        </button>
+                    </div>
+                </li>
+                <li class="todo__item">
+                    <div class="todo__container">
+                        <input
+                            type="checkbox"
+                            name="todo-checkbox"
+                            id="todo-checkbox"
+                            class="todo__checkbox"
+                            title="Отметить выполнение"
+                        />
+                        <p class="todo__content">
+                            Далеко-далеко за словесными горами в стране.
+                        </p>
+                        <button
+                            type="button"
+                            class="todo__delete button"
+                            id="todo__delete"
+                        >
+                            УДАЛИТЬ
+                        </button>
                     </div>
                 </li>
             </ul>
@@ -37,18 +86,19 @@
     align-items: center;
     width: 100vw;
     height: 100vh;
+    padding: 15px;
     background-color: #dcefff6b;
     // .todo__inner
 
     &__inner {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 100%;
-      max-width: 520px;
-      padding: clamp(25px, 2.70vw, 52px);
-      border-radius: 25px;
-      background-color: #fff;
+        display: flex;
+        flex-direction: column;
+        row-gap: calc(var(--gap-12) * 2);
+        width: 100%;
+        max-width: 720px;
+        padding: clamp(25px, 2.7vw, 52px);
+        border-radius: 25px;
+        background-color: #fff;
     }
 
     // .todo__task
@@ -59,21 +109,34 @@
     // .todo__list
 
     &__list {
+      display: flex;
+      flex-direction: column;
+      row-gap: var(--gap-12);
     }
 
     // .todo__item
 
     &__item {
+      display: block;
+      border-radius: 12px;
+      padding: 12px;
+      background-color: rgba(255, 130, 130, 0.411);      
     }
 
     // .todo__container
 
     &__container {
+      display: flex;
+      align-items: center;
+      flex-flow: row wrap;
+      gap: var(--gap-12);
     }
 
     // .todo__checkbox
 
     &__checkbox {
+      width: 16px;
+      height: 16px;
     }
 
     // .todo__content
@@ -84,14 +147,39 @@
     // .todo__delete
 
     &__delete {
+      margin-left: auto;
     }
 }
 .task {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: row wrap;
+    gap: var(--gap-12);
+    width: 100%;
+
     // .task__input
 
     &__input {
+        padding: var(--gap-12) 10px;
+        flex-grow: 1;
+        border: none;
+        border-bottom: 2px solid #3b3b3b;
+        outline: none;
+        font-size: 20px;
+        font-weight: 400;
+        &:focus {
+            border-color: #afe6ff;
+        }
     }
 
+    // .task__action
+
+    &__action {
+        display: flex;
+        align-items: center;
+        gap: var(--gap-12);
+    }
     // .task__add
 
     &__add {
